bc49d533a46e9132d7b6d70298e66973ec2caa75
nrholm1 <nielsraunkjaer@gmail.com>
2020-12-12 15:32:16 +0100

    Add Server logic for listening to connections and receiving objectsa

--- src/Networking/Server/Server.java
+++ src/Networking/Server/Server.java
@@ -1,20 +1,58 @@
 package Networking.Server;
 
+import javax.net.ServerSocketFactory;
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 // thread that manages one socket connection with a client
 public class Server extends Thread {
-    int PORT;
+    public int PORT;
+    private ServerSocket socket;
     ObjectInputStream objIn;
     ObjectOutputStream objOut;
 
-    public Server(int _port) {
+    public Server(int _port) throws IOException {
+        System.out.println("Starting Server");
         PORT = _port;
+        socket = ServerSocketFactory.getDefault().createServerSocket(_port);
     }
 
     @Override
     public synchronized void start() {
+        while (true) {
+            try {
+                final Socket socketToClient = socket.accept();
+                ClientHandler clientHandler = new ClientHandler(socketToClient);
+                clientHandler.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    private class ClientHandler extends Thread {
+        private Socket socket;
+        ObjectInputStream inputStream;
+
+        public ClientHandler(Socket _socket) throws IOException {
+            socket = _socket;
+            inputStream = new ObjectInputStream(socket.getInputStream());
+        }
+
+        @Override
+        public void start() {
+            while (true) {
+                try {
+                    Object o = inputStream.readObject();
+                    System.out.println(o);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
     }
 }
--- src/Networking/TestDriver.java
+++ src/Networking/TestDriver.java
@@ -1,15 +1,39 @@
 package Networking;
 
+import Networking.Server.Server;
 import Networking.Utils.Payload;
 import Networking.Utils.PayloadBuilder;
 
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
 public class TestDriver {
-    public static void main(String args[]) throws InterruptedException {
+    public static void main(String args[]) throws InterruptedException, IOException {
         Payload payload = new PayloadBuilder()
                 .withX(69)
                 .withY(420)
                 .withCurrInput(3)
                 .create();
         System.out.println(payload);
+
+
+
+        Server server = new Server(5050);
+        server.start();
+
+        Socket socketToServer = new Socket("localhost", 5050);
+        ObjectOutputStream outStream = new ObjectOutputStream(socketToServer.getOutputStream());
+
+        for (int i=1; i<10; i++) {
+            try {
+                Thread.sleep((long) (Math.random()*100));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Sending object to server ...");
+            outStream.writeObject("test message #"+i);
+        }
+        System.exit(0);
     }
 }
